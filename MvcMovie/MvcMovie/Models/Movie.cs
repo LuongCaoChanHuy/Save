681d49d479b3f1026c54f1d9fd87a89a78132941
unknown <luongcaochanhuy181@gmail.com>
2020-10-21 22:05:12 +0700

    add MvcMovie

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+         public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Genre genre { get; set; }
+
+    }
+}
